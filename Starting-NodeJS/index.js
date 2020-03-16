64ecc0bcd846a505ca0c62e13efc4ad4bd2062b0
Vincent Bouton <vincent.bouton.dev@gmail.com>
2020-03-16 15:40:21 +0100

    Add asynchronus way to read or write file

--- Starting-NodeJS/index.js
+++ Starting-NodeJS/index.js
@@ -1,9 +1,27 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+// Blocking, Synchronus way
 
-console.log(textIn)
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+//
+// const textOut = `this is what we know about avocado ${textIn}.\nCreate on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written");
 
-const textOut = `this is what we know about avocado ${textIn}.\nCreate on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt',textOut)
-console.log('File written')
+// Non-Blocking, Asynchronus way
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("Error !");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
+        console.log("Your file has been written ✍");
+      });
+    });
+  });
+});
+console.log("Will read file");
