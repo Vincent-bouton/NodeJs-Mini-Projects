87ad56becf4ffeaffbfcbceb1af218b0b8c1fd74
Vincent Bouton <vincent.bouton.dev@gmail.com>
2020-03-16 16:25:23 +0100

    Add server + pathName

--- Starting-NodeJS/index.js
+++ Starting-NodeJS/index.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-//
+// Files
 
 // Blocking, Synchronus way
 
@@ -11,17 +16,39 @@ const fs = require("fs");
 
 // Non-Blocking, Asynchronus way
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("Error !");
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("Error !");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
+//         console.log("Your file has been written ✍");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file");
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
-        console.log("Your file has been written ✍");
-      });
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-//
+// Server
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is overview");
+  } else if (pathName === "/product") {
+    res.end("This is product");
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world"
     });
-  });
+    res.end("<h1>404</h1>");
+  }
 });
-console.log("Will read file");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
