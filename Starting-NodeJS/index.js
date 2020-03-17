dfa37423f3563f1b4fce99332db68f76b7f1234c
Vincent Bouton <vincent.bouton.dev@gmail.com>
2020-03-17 16:06:30 +0100

    render the overview page

--- Starting-NodeJS/index.js
+++ Starting-NodeJS/index.js
@@ -33,6 +33,33 @@ const url = require("url");
 
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-//
 // Server
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
@@ -42,8 +69,12 @@ const server = http.createServer((req, res) => {
 
   //OVERVIEW PAGE
   if (pathName === "/" || pathName === "/overview") {
-    fs.re;
-    res.end("This is overview");
+    res.writeHead(200, { "content-type": "text/html" });
+    const cardsHtml = dataObject
+      .map(el => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
 
     // PRODUCT PAGE
   } else if (pathName === "/product") {
