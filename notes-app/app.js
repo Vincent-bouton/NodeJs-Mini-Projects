da4ae28b048d793dc9b56b8b9c8973e056d5c3e4
Vincent <Vincent.bouton.dev@gmail.com>
2020-09-22 03:51:27 +0200

    Argument parsing with Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,57 @@
-const chalk = require('chalk')
-const getNotes = require("./notes")
+const chalk = require("chalk");
+const yargs = require("yargs");
 
-const log = console.log
-const error = chalk.red.inverse.bold
+const getNotes = require("./notes");
 
-const command = process.argv[2]
+// CHALK COLOR
+const log = console.log;
+const error = chalk.red.inverse.bold;
 
-if(command === 'add'){
-    log('Adding note!')
-}else if(command === 'remove'){
-    log('Removing note!')
-}else{
-    log(error('ERROR: Choose between "add" or "remove"'))
-}
+// Customize yargs version
+yargs.version("1.1.0");
+
+// add, remove, read, list
+//create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: "true",
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    log("Title: ", argv.title);
+    log("Body: ", argv.body);
+  },
+});
+
+//create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: () => log("Removing the note!"),
+});
+
+//create remove command
+yargs.command({
+  command: "list",
+  describe: "list your notes",
+  handler: () => log("listing out all notes!"),
+});
+
+//create remove command
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: () => log("Reading the note!"),
+});
+
+yargs.parse();
