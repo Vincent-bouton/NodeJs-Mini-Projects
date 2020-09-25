17b396a9e54e607b76ffbc49571ccfe75f23393b
Vincent <Vincent.bouton.dev@gmail.com>
2020-09-25 20:40:58 +0200

    Customizing HTTP Requests

--- wheather-app/app.js
+++ wheather-app/app.js
@@ -3,7 +3,8 @@ const request = require("postman-request");
 const url =
   "http://api.weatherstack.com/current?access_key=c05df93ddaeecea354dd377eeffc4aec&query=brussels";
 
-request(url, (err, res) => {
-  const data = JSON.parse(res.body);
-  console.log(data.current);
+request({ url: url, json: true }, (err, res) => {
+  console.log(
+    `${res.body.current.weather_descriptions[0]}. it is currently ${res.body.current.temperature} degress out. it feels like ${res.body.current.feelslike} degress out.`
+  );
 });
