1f640906e89ae20f4dd5a37ed2d708613fbba335
Vincent <Vincent.bouton.dev@gmail.com>
2020-09-27 22:37:05 +0200

    Handling errors

--- wheather-app/app.js
+++ wheather-app/app.js
@@ -4,16 +4,28 @@ const url =
   "http://api.weatherstack.com/current?access_key=c05df93ddaeecea354dd377eeffc4aec&query=brussels";
 
 request({ url: url, json: true }, (err, res) => {
-  console.log(
-    `${res.body.current.weather_descriptions[0]}. it is currently ${res.body.current.temperature} degress out. it feels like ${res.body.current.feelslike} degress out.`
-  );
+  if (err) {
+    console.log("Unable to connect to weather service");
+  } else if (res.body.error) {
+    console.log(res.body.error.info);
+  } else {
+    console.log(
+      `${res.body.current.weather_descriptions[0]}. it is currently ${res.body.current.temperature} degress out. it feels like ${res.body.current.feelslike} degress out.`
+    );
+  }
 });
 
 const geocodeURL =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidmluY2VudC1yaXNrMCIsImEiOiJja2ZqeW04cmIxOXY1MnFzMmg5ZjR6cWg5In0.2z8lz29P4OCUdMqVOkocMQ&limit=1";
 
 request({ url: geocodeURL, json: true }, (err, res) => {
-  const lat = res.body.features[0].center[1];
-  const long = res.body.features[0].center[0];
-  console.log("lat : " + lat + " long : " + long);
+  if (err) {
+    console.log("Unable to connect to Geocode service");
+  } else if (res.body.features.length === 0) {
+    console.log("Don't find the city");
+  } else {
+    const lat = res.body.features[0].center[1];
+    const long = res.body.features[0].center[0];
+    console.log("lat : " + lat + " long : " + long);
+  }
 });
