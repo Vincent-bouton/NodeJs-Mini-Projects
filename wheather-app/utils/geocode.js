8d15a612724121e2b561019e1a5b9acb79cfbc01
Vincent <Vincent.bouton.dev@gmail.com>
2020-09-30 23:00:12 +0200

    Callback Abstraction

---
+++ wheather-app/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("postman-request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(address) +
+    ".json?access_token=pk.eyJ1IjoidmluY2VudC1yaXNrMCIsImEiOiJja2ZqeW04cmIxOXY1MnFzMmg5ZjR6cWg5In0.2z8lz29P4OCUdMqVOkocMQ&limit=1";
+
+  request({ url: url, json: true }, (err, res) => {
+    if (err) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (res.body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        lat: res.body.features[0].center[1],
+        long: res.body.features[0].center[0],
+        location: res.body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
